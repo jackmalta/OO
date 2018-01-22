559cf7d7a44dbcaa274c035823fff645a9735354
php 09 <php.09@pedrita.cec.ime.usp.br>
2018-01-22 19:52:10 -0200

    Segundo Commit do cartão

--- cartao.php
+++ cartao.php
@@ -9,6 +9,13 @@
   $cliente -> numero = "123456789";
   $cliente -> dataDeValidade = "12/26";
 
-  echo "O número de seu cartão é: $cliente->numero \n A validade é até: $cliente->dataDeValidade";
+  $cliente2 = new Cartao();
+  $cliente2 -> numero = "987654321";
+  $cliente2 -> dataDeValidade = "10/22";
+
+
+  echo "O número de seu cartão é: $cliente->numero \n A validade é até: $cliente->dataDeValidade".PHP_EOL;
+  echo "O Cliente 2 tem o cartão de número: $cliente2->numero \ne a validade é até: $cliente2->dataDeValidade".PHP_EOL;
+
 
  ?>
