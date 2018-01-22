d3c6ab0285d266f7280df65fce63c96bc89f4ff4
php 09 <php.09@pedrita.cec.ime.usp.br>
2018-01-22 20:12:16 -0200

    Escola e alunos e Funcionários

--- escola.php
+++ escola.php
@@ -5,7 +5,11 @@
     public $rg;
     public $dataNascimento;
   }
-
+  class Funcionarios
+  {
+    public $nome;
+    public $salario;
+  }
 
 //Primeiro Aluno
   $Aluno1 = new Aluno();
@@ -13,12 +17,21 @@
   $Aluno1 -> rg = "428798541";
   $Aluno1 -> dataNascimento = "18/8/1978";
 
-
 //Segundo Aluno
   $Aluno2 = new Aluno();
   $Aluno2 -> nome = "Michael Scott";
   $Aluno2 -> rg = "258795413";
   $Aluno2 -> dataNascimento = "13/06/1969";
+
+//Primeiro Funcionário
+  $Funcionario1 = new Funcionarios();
+  $Funcionario1 -> nome = "Pam Beemsley";
+  $Funcionario1 -> salario = "1900";
+
+//Segundo Funcionário
+  $Funcionario2 = new Funcionarios();
+  $Funcionario2 -> nome = "Stanley Brown";
+  $Funcionario2 -> salario = "1600";
 
 
   echo "Os dados do Primeiro aluno são: \n
@@ -26,5 +39,8 @@
   echo "Os dados do Segundo aluno são: \n
     Nome: $Aluno2->nome \tRG: $Aluno2->rg \tData de Nascimento: $Aluno2->dataNascimento \n".PHP_EOL;
 
-
+    echo "Os dados de nosso primeiro funcionário são: \n
+    Nome: $Funcionario1->nome \tSalário: $Funcionario1->salario \n".PHP_EOL;
+    echo "Os dados de nosso Segundo funcionário são: \n
+    Nome: $Funcionario2->nome \tSalário: $Funcionario2->salario \n".PHP_EOL;
  ?>
