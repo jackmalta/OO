0b894a463ec3f8ef0405c13a7a97f035098203c0
php 09 <php.09@pedrita.cec.ime.usp.br>
2018-01-22 20:02:29 -0200

    Adicionando a classe Conta

--- cartao.php
+++ cartao.php
@@ -5,18 +5,34 @@
     public $dataDeValidade;
   }
 
+  class Conta
+  {
+    public $numero;
+    public $saldo;
+    public $limite;
+  }
+
+
   class Agencia
   {
     public $numero;
   }
 
+//Primeiro Cliente
   $cliente = new Cartao();
   $cliente -> numero = "123456789";
   $cliente -> dataDeValidade = "12/26";
 
+  $C1Conta = new Conta();
+  $C1Conta -> numero = "54187952";
+  $C1Conta -> saldo = "1500";
+  $C1Conta -> limite = "3000";
+
   $clienteAg = new Agencia();
   $clienteAg -> numero = "125";
 
+
+//Segundo Cliente
   $cliente2Ag = new Agencia();
   $cliente2Ag -> numero = "124";
 
@@ -24,9 +40,18 @@
   $cliente2 -> numero = "987654321";
   $cliente2 -> dataDeValidade = "10/22";
 
+  $C2Conta = new Conta();
+  $C2Conta -> numero = "36987451";
+  $C2Conta -> saldo = "70";
+  $C2Conta -> limite = "7000";
 
-  echo "O número de seu cartão é: $cliente->numero \n A validade é até: $cliente->dataDeValidade \nSua Agência é: $clienteAg->numero".PHP_EOL;
-  echo "O Cliente 2 tem o cartão de número: $cliente2->numero \ne a validade é até: $cliente2->dataDeValidade \nSua Aguência é: $cliente2Ag->numero".PHP_EOL;
+
+  echo "O número de seu cartão é: $cliente->numero \n A validade é até: $cliente->dataDeValidade \nSua Agência é: $clienteAg->numero
+  Os dados de sua conta \nNúmero: $C1Conta->numero \tSaldo: $C1Conta->saldo \tLimite: $C1Conta->limite  \n".PHP_EOL;
+
+
+  echo "O Cliente 2 tem o cartão de número: $cliente2->numero \ne a validade é até: $cliente2->dataDeValidade \nSua Aguência é: $cliente2Ag->numero
+  Os dados de sua conta \nNúmero: $C2Conta->numero \tSaldo: $C2Conta->saldo \tLimite: $C2Conta->limite  ".PHP_EOL;
 
 
  ?>
