38cbdd135d0550ff814acac5a2abe52f4e01c944
php 09 <php.09@pedrita.cec.ime.usp.br>
2018-01-22 19:55:49 -0200

    Adicionando a classe Agência

--- cartao.php
+++ cartao.php
@@ -5,17 +5,28 @@
     public $dataDeValidade;
   }
 
+  class Agencia
+  {
+    public $numero;
+  }
+
   $cliente = new Cartao();
   $cliente -> numero = "123456789";
   $cliente -> dataDeValidade = "12/26";
+
+  $clienteAg = new Agencia();
+  $clienteAg -> numero = "125";
+
+  $cliente2Ag = new Agencia();
+  $cliente2Ag -> numero = "124";
 
   $cliente2 = new Cartao();
   $cliente2 -> numero = "987654321";
   $cliente2 -> dataDeValidade = "10/22";
 
 
-  echo "O número de seu cartão é: $cliente->numero \n A validade é até: $cliente->dataDeValidade".PHP_EOL;
-  echo "O Cliente 2 tem o cartão de número: $cliente2->numero \ne a validade é até: $cliente2->dataDeValidade".PHP_EOL;
+  echo "O número de seu cartão é: $cliente->numero \n A validade é até: $cliente->dataDeValidade \nSua Agência é: $clienteAg->numero".PHP_EOL;
+  echo "O Cliente 2 tem o cartão de número: $cliente2->numero \ne a validade é até: $cliente2->dataDeValidade \nSua Aguência é: $cliente2Ag->numero".PHP_EOL;
 
 
  ?>
