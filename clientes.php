168aec1d318c00be880d12e40ddad38722efb88a
php 09 <php.09@pedrita.cec.ime.usp.br>
2018-01-22 19:39:22 -0200

    Clientes

---
+++ clientes.php
@@ -0,0 +1,15 @@
+		<?php
+		class Cliente
+		{
+			public $nome;
+			public $codigo;
+		}
+
+		$data = new Cliente();
+		$data -> nome = "Juca da silva";
+		$data -> codigo = "123456";
+
+		echo "\nO nome do cliente é: $data->nome \t código: $data->codigo";
+
+
+		 ?>
